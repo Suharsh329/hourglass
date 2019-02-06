1423db0dfe8cdf89c4a60d08f2935db1ce246174
Suharsh Nagasampagi <suharsh329@gmail.com>
2019-02-06 12:44:34 +0530

    Add dependency injection and getValidatedBoards function call

--- src/Commands/TaskCommand.php
+++ src/Commands/TaskCommand.php
@@ -2,7 +2,7 @@
 
 namespace App\Commands;
 
-use App\Helpers\Create;
+use App\Helpers\TaskNote;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,6 +12,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 class TaskCommand extends Command
 {
     protected  static $defaultName = 'task';
+    protected $task;
+
+    public function __construct(TaskNote $note)
+    {
+        $this->task = $note;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
@@ -33,66 +41,44 @@ class TaskCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $task = implode(' ', $input->getArgument('task_description'));
+        $taskDescription = implode(' ', $input->getArgument('task_description'));
 
+        // Default board is Main
         $boards = ['Main'];
 
         if ($input->getOption('board')) {
-            $boards = explode(',', implode(",", $input->getOption('board')));
-        }
-
-        foreach ($boards as $board) {
-            if (!$this->isValidInput($board, 'board')) {
+            $boards = $this->task->getValidatedBoards($input->getOption('board'));
+            if(empty($boards)) {
                 $output->writeln("<comment>Please enter a valid board name</comment>");
                 return;
             }
         }
 
-        $boards = $this->sanitizeBoards($boards);
-
+        // Default due date is indefinite
         $due = 'Indefinite';
 
+        // If user enters a number with the due date flag
         if ($input->getOption('due')) {
-            if (!$this->isValidInput($input->getOption('due'), 'due')) {
+            if (!$this->task->isValidInput($input->getOption('due'), 'due')) {
                 $output->writeln("<comment>Please enter a valid number</comment>");
                 return;
             }
-            $due = $input->getOption('due');
+            // Convert the due date from input number to actual date
+            $due = $this->task->getDueDate(date('jS F Y'), $input->getOption('due'));
         }
 
         $task = [
-            'task' => $task,
+            'description' => $taskDescription,
             'boards' => $boards,
             'due' => $due,
-            'date' => date('jS F Y') // 1st February 2019
+            'date' => date('jS F Y'), // Today's date e.g 1st February 2019
+            'type' => 'task'
         ];
 
-        $output->writeln("<info>Task created: " . $task['task'] ." </info>");
-    }
-
-    protected function isValidInput(string $value, string $type)
-    {
-        // Check if board name is valid
-        // Alphanumeric names with hyphens and underscores are allowed
-        if ($type === 'board') {
-            $aValid = ['-', '_'];
-            return ctype_alnum(str_replace($aValid, '', $value));
+        if ($this->task->createTask($task)) {
+            $output->writeln("<info>Task created</info>");
+        } else {
+            $output->writeln("<comment>Task could not created</comment>");
         }
-
-        // Check if due date is a valid number
-        return ctype_digit($value);
-    }
-
-    protected function sanitizeBoards(array $boards): array
-    {
-        $temp = array_map(function($board) {
-            $val = strtolower($board);
-            if($val == "main") {
-                $val = "Main";
-            }
-            return $val;
-        }, $boards);
-
-        return array_unique($temp);
     }
 }
