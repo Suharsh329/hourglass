4a6c9c77524620efd6b9c7e01a658c02f61b8cd9
Suharsh Nagasampagi <suharsh329@gmail.com>
2019-06-04 19:58:26 +0530

    Initial commit

---
+++ src/Commands/MoveCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Commands;
+
+
+use App\Helpers\Update;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MoveCommand extends Command
+{
+
+    protected static $defaultName = 'move';
+    private $update;
+
+    public function __construct(Update $update)
+    {
+        $this->update = $update;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Moves entries from one board to another')
+            ->addArgument('id', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Task or note id')
+            ->addOption(
+                'board',
+                'b',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'Specify which board the task belongs to. A new board will be created if it does not exist.'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $id = explode(',', implode(',', $input->getArgument('id')));
+
+        // Default board is Main
+        $boards = ['Main'];
+
+        if ($input->getOption('board')) {
+            $boards = $this->update->getValidatedBoards($input->getOption('board'));
+            if(empty($boards)) {
+                $output->writeln("<comment>Please enter a valid board name</comment>");
+                return;
+            }
+        }
+
+        if (count($boards) !== 2) {
+            $output->writeln("<comment>Please enter 2 boards</comment>");
+            return;
+        }
+
+        if ($this->update->move($id, $boards[0], $boards[1])) {
+            $output->writeln("<info>Entries moved</info>");
+        } else {
+            $output->writeln("<comment>Entries could not be moved</comment>");
+        }
+    }
+}
