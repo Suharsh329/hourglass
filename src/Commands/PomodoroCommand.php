9caed5f18677534701116a1c18aee53a1cb8b78c
Suharsh Nagasampagi <suharsh329@gmail.com>
2019-06-07 22:56:26 +0530

    Add Pomodoro Helper object and options for command

--- src/Commands/PomodoroCommand.php
+++ src/Commands/PomodoroCommand.php
@@ -2,49 +2,87 @@
 
 namespace App\Commands;
 
+use App\Helpers\Pomodoro;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\ProgressBar;
 
 class PomodoroCommand extends Command
 {
     protected static $defaultName = 'omodoro';
 
+    protected $pomodoro;
+
+    public function __construct(Pomodoro $pomodoro)
+    {
+        $this->pomodoro = $pomodoro;
+        parent::__construct();
+    }
+
     protected function configure()
     {
-        $this->setDescription('Starts a pomodoro timer');
+        $this->setDescription('Starts a pomodoro timer')
+            ->addArgument('task', InputArgument::OPTIONAL, 'Task id')
+            ->addOption(
+                'time',
+                't',
+                InputOption::VALUE_OPTIONAL,
+                'Specify activity time'
+            )
+            ->addOption(
+                'rest',
+                'r',
+                InputOption::VALUE_OPTIONAL,
+                'Specify rest break time'
+            )
+            ->addOption(
+                'board',
+                'b',
+                InputOption::VALUE_OPTIONAL,
+                'Specify which board the task belongs to'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $activityTime = 25 * 60 * 10;
-        $progressBar = new ProgressBar($output, $activityTime);
-        $progressBar->setFormat(
-            "<fg=cyan;bg=default>Activity - %status:-45s%</>\n %bar%\n%elapsed%\n"
-        );
-        $progressBar->setBarCharacter('<fg=green>⚬</>');
-        $progressBar->setEmptyBarCharacter("<fg=red>⚬</>");
-        $progressBar->setProgressCharacter("<fg=green>➤</>");
-        $progressBar->setBarWidth(25);
+        $task = 'Activity';
+        if ($input->getArgument('task')) {
+            $task = $this->pomodoro->getTask($input->getArgument('task'));
+        }
 
-        $progressBar->setRedrawFrequency(1);
+        $aTime = 25;
+        if ($input->getOption('time')) {
+            $aTime = $input->getOption('time');
+        }
+
+        $rTime = 5;
+        if ($input->getOption('rest')) {
+            $rTime = $input->getOption('rest');
+        }
+
+        $activityTime = $aTime * 60 * 10;
+
+        $breakTime = $rTime * 60 * 10;
+
+        $progressBar = $this->pomodoro->initProgressBar($output, $activityTime, $aTime);
+
         $progressBar->start();
         for ($i = 0; $i < $activityTime; $i++) {
             if ($i <= $activityTime / 2) {
-                $progressBar->setMessage("Started", 'status');
+                $progressBar->setMessage($task . "Started", 'status');
             } elseif ($i > $activityTime / 2 && $i <= 3 * $activityTime / 4) {
-                $progressBar->setMessage("Halfway there", 'status');
+                $progressBar->setMessage($task . "Halfway there", 'status');
             } else {
-                $progressBar->setMessage("Almost there", 'status');
+                $progressBar->setMessage($task . "Almost there", 'status');
             }
             $progressBar->advance();
             usleep(100000);
         }
         $progressBar->setMessage("Completed", 'status');
         $progressBar->finish();
+
         $output->writeln("");
     }
 }
